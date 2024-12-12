37b952403791c95aa5f12748519c626e1b7e556e
Tarun Jindal <101278379+itstarunjindal@users.noreply.github.com>
2024-12-12 14:32:28 +0530

    [tarun] ADD: Send message using a method send_notification

---
+++ oops/message_system.rb
@@ -0,0 +1,28 @@
+class Message
+  def send_message
+    raise NotImplementedError, "#{self.class} must have send_message method"
+  end
+end
+
+class EmailMessage < Message
+  def send_message
+    puts "Email message sent"
+  end
+end
+
+class SMSMessage < Message
+  def send_message
+    puts "SMS message sent"
+  end
+end
+
+def send_notification(message)
+  message.send_message
+end
+
+email = EmailMessage.new
+sms = SMSMessage.new
+
+
+send_notification(email)
+send_notification(sms)
