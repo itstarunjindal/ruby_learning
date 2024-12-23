44b86eec0a2142ebe9b5f021cfe62f5d5d2efd5f
Tarun Jindal <101278379+itstarunjindal@users.noreply.github.com>
2024-12-23 14:42:26 +0530

    [tarun]ADD: Finding max subarray using Kadane’s Algorithm

--- dsa/Searching/max_subarray.rb
+++ dsa/Searching/max_subarray.rb
@@ -1,13 +1,13 @@
 def max_subarray_sum(arr)
-  max_current = arr[0]
-  max_global = arr[0]
+  current = arr[0]
+  max = arr[0]
 
   arr[1..-1].each do |num|
-    max_current = [num, max_current + num].max
-    max_global = [max_global, max_current].max
+    current = [num, current + num].max
+    max = [max, current].max
   end
 
-  max_global
+  max
 end
 
 arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
